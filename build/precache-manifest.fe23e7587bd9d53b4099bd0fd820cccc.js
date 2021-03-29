9a3ae47a322424d33d644fd2ac7f5165240e2ea5
Bhavesh <63704323+proBhavesh@users.noreply.github.com>
2021-03-29 19:42:41 +0530

    FInal Product

---
+++ build/precache-manifest.fe23e7587bd9d53b4099bd0fd820cccc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33984ddb4ab631449071a982dd0f974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce25a147f20ed05fb6d",
+    "url": "/static/js/2.7ce38902.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.7ce38902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e5259614a3732254f7",
+    "url": "/static/js/main.c5d8b420.chunk.js"
+  },
+  {
+    "revision": "2085bf0da2d0457c1be3",
+    "url": "/static/js/runtime-main.2a51fc9d.js"
+  },
+  {
+    "revision": "f85903d473cb19505e2aa6c63a0db00b",
+    "url": "/static/media/athlete-small.f85903d4.png"
+  },
+  {
+    "revision": "6d53898d42640dad92347101b160da7f",
+    "url": "/static/media/athlete2.6d53898d.png"
+  },
+  {
+    "revision": "f5c64a5f820c4ac31423ed0ee5d95c7c",
+    "url": "/static/media/clock.f5c64a5f.svg"
+  },
+  {
+    "revision": "f620483184a3efa896127ee80cab531c",
+    "url": "/static/media/diaphragm.f6204831.svg"
+  },
+  {
+    "revision": "e149f59c6323b18f98e987cd564e1fc9",
+    "url": "/static/media/good-times2.e149f59c.jpg"
+  },
+  {
+    "revision": "169a9b3ae8eff8e916dc7711c7546c64",
+    "url": "/static/media/goodtimes-small.169a9b3a.png"
+  },
+  {
+    "revision": "99a18d77af2a0e70784a842789ab9a34",
+    "url": "/static/media/home1.99a18d77.png"
+  },
+  {
+    "revision": "ee6a60799cceb9c60ce67b133ca6574c",
+    "url": "/static/media/home2.ee6a6079.png"
+  },
+  {
+    "revision": "d1749735b44aafa17334e7710263aac3",
+    "url": "/static/media/money.d1749735.svg"
+  },
+  {
+    "revision": "8c795ad57e55b06d76b8a73b27a13fd1",
+    "url": "/static/media/teamwork.8c795ad5.svg"
+  },
+  {
+    "revision": "74873026dbed7229712447d165a7a2fd",
+    "url": "/static/media/the-racer2.74873026.jpg"
+  },
+  {
+    "revision": "ca518f44c6e1ddc450654002971bb467",
+    "url": "/static/media/theracer-small.ca518f44.png"
+  }
+]);
